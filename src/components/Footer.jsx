8c4ad2a010f02407215704afb2aca9df66002ab3
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2023-11-19 13:00:15 +0530

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <div className='container-max text-center py-4 border-t'>
+        🍔 FoodHunt, made by{' '}
+        <a
+          href='https://github.com/Vikash0122/Foodie'
+          target={'_blank'}
+          className='text-orange-500'
+        >
+          Vikash Kumar
+        </a>
+      </div>
+    );
+  };
+  export default Footer;
